79deaa9528a2f92a7316b07e4fd50b014adc9835
Jacob Koziej <jacobkoziej@gmail.com>
2022-12-23 23:43:39 -0500

    Add `version.sh`

---
+++ bin/version.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# version.sh -- report book version
+# Copyright (C) 2022  Jacob Koziej <jacobkoziej@gmail.com>
+
+VERSION=$(git describe --always --broken --dirty --tags 2> /dev/null)
+
+echo "${VERSION:-v0.0.0}"
