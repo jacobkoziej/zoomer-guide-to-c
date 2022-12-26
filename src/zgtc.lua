ba423b9efe8c78211c2f5636f4e7a7da3e5f94ed
Jacob Koziej <jacobkoziej@gmail.com>
2022-12-26 01:32:29 -0500

    Define `zgtc.lines()`
    The function returns the number of lines in a file excluding licensing
    information.

--- src/zgtc.lua
+++ src/zgtc.lua
@@ -5,4 +5,19 @@
 
 local zgtc = { }
 
+function zgtc.lines(path)
+	local path = kpse.find_file(path)
+
+	local lines = 0
+	for line in io.lines(path) do
+		if line:match('.*SPDX%-License%-Identifier:.*') then
+			lines = lines - 1
+			break
+		end
+		lines = lines + 1
+	end
+
+	return lines
+end
+
 return zgtc
