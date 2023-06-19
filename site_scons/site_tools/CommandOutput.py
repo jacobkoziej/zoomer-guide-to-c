cf00bb67bc30dc4c1bc954e9cfcdde054a1305af
Jacob Koziej <jacobkoziej@gmail.com>
2023-06-18 21:40:02 -0700

    Add support for mocking input command

--- site_scons/site_tools/CommandOutput.py
+++ site_scons/site_tools/CommandOutput.py
@@ -4,7 +4,9 @@
 #
 # Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
 
-import SCons
+from os.path import basename
+
+from SCons.Builder import Builder
 
 
 def emitter(target, source, env):
@@ -12,13 +14,15 @@ def emitter(target, source, env):
 
 
 def generator(target, source, env, for_signature):
+    command = basename(source[1].path) if len(source) >= 2 else basename(source[0].path)
+
     FORMAT_BOLD = '\\033[1m'
     FORMAT_NONE = '\\033[0m'
 
     commands = [ ]
 
     commands += [f'printf \'%b$ %b\' \'{FORMAT_BOLD}\' \'{FORMAT_NONE}\' > $TARGET.file']
-    commands += [f'cat $SOURCE.file >> $TARGET.file']
+    commands += [f'cat {command} >> $TARGET.file']
     commands += [f'script --flush --return --quiet --command \'$$(cat $SOURCE.file)\' >> $TARGET.file || true']
 
     return ['cd $SOURCE.dir && ' + command for command in commands]
@@ -28,7 +32,7 @@ def generate(env):
     if env.Detect('CommandOutput'):
         return
 
-    builder = SCons.Builder.Builder(emitter=emitter, generator=generator)
+    builder = Builder(emitter=emitter, generator=generator)
 
     env['BUILDERS']['CommandOutput'] = builder
 
