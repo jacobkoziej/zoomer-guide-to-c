3bb854e4d4ced1ab10f1fba7644defb7af755638
Jacob Koziej <jacobkoziej@gmail.com>
2023-06-18 21:43:03 -0700

    Add blurb about going off the deep end

---
+++ src/hello-world.d/compile-error-mock.c
@@ -0,0 +1,18 @@
+#line 1 "hello-world.c"
+#include <stdio.h>
+
+
+int main(void)
+{
+	int epoch = 1970
+
+	printf("Hello %f's World!\n", epoch);
+
+	return 0;
+}
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * compile-error-mock.c -- everyone's favorite timeless mock?
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
