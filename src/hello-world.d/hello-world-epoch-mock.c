00c2804e2c08d248d88bcc3b6a47ae60373c9b42
Jacob Koziej <jacobkoziej@gmail.com>
2023-06-21 00:15:04 -0700

    Add `Specifying a C Standard` subsubsection

---
+++ src/hello-world.d/hello-world-epoch-mock.c
@@ -0,0 +1,18 @@
+#line 1 "hello-world.c"
+#include <stdio.h>
+
+
+int main(void)
+{
+	int epoch = 1970;
+
+	printf("Hello %d's World!\n", epoch);
+
+	return 0;
+}
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * hello-world-epoch.c -- everyone's favorite epoch mock?
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
---
+++ src/hello-world.d/hello-world-epoch.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int epoch = 1970;
+
+	printf("Hello %d's World!\n", epoch);
+
+	return 0;
+}
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * hello-world-epoch.c -- everyone's favorite epoch?
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
