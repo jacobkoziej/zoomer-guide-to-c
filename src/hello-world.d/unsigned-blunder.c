79659af2f59a196937b1a43adc8b651f3e0092eb
Jacob Koziej <jacobkoziej@gmail.com>
2023-06-25 16:30:01 -0700

    Add 'Runtime Errors` subsection

---
+++ src/hello-world.d/unsigned-blunder.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("Hello %u!\n", -1);
+
+	return 0;
+}
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * unsigned-blunder.c -- mixing up the format character
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
